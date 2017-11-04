8118ad2b5029c9107ae2764a8f2cb05d0826b60d
Richard Sbresny <voidet@nightgen.com>
2017-11-03 20:53:34 -0700

    (use "git reset HEAD <file>..." to unstage) 	modified:   _site Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git checkout -- <file>..." to discard changes in working directory)   (commit or discard the untracked or modified content in submodules) 	modified:   _site (modified content, untracked content)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-git add .
-
+jekyll build
 OUTPUT="$(git -c color.status=false status \
 | sed -n -e '1,/Changes to be committed:/ d' \
             -e '1,1 d' \
             -e '/^Untracked files:/,$ d' \
             -e 's/^\s*//' \
             -e '/./p')"
-git commit -m "${OUTPUT}"
 
+cd _site
+git checkout master
+git add -u .
+git commit -m "${OUTPUT}"
 git push origin
-jekyll build
-ssh -T -p 9092 wumpus@ssh.jotlab.com < ./bin/remote-manageering.sh
+
+cd ../
+git add .
+git commit -m "${OUTPUT}"
+git push origin
+
+# ssh -T -p 9092 wumpus@ssh.jotlab.com < ./bin/remote-manageering.sh
